f24453fe990f9fca8798244e3f9cb6e43de17e08
Pramod Ghimire <pramodghimire96@gmail.com>
2025-12-07 20:46:13 +0545

    Added User CRUD

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entity/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(userData: CreateUserDto) {
+    const user = this.userRepository.create(userData);
+    return this.userRepository.save(user);
+  }
+
+  async getUserById(id: number) {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async getUsers() {
+    return this.userRepository.find();
+  }
+
+  async updateUser(id: number, userData: UpdateUserDto) {
+    await this.userRepository.update(id, userData);
+    return this.getUserById(id);
+  }
+
+  async deleteUser(id: number) {
+    await this.userRepository.delete(id);
+  }
+}
